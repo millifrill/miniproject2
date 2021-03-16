42e68a08f40b6f491381fa57699475aa8104a550
jv98 <jacob@vahlen.se>
2021-03-16 15:17:55 +0100

    start på code

--- src/components/detailedView/detailedView.tsx
+++ src/components/detailedView/detailedView.tsx
@@ -1,1 +1,11 @@
 import React from 'react';
+
+
+
+export default function DetailedView() {
+    return(
+        <div>
+
+        </div>
+    )
+}
--- src/components/detailedView/infoTable.tsx
+++ src/components/detailedView/infoTable.tsx
@@ -1,1 +1,7 @@
 import React from 'react';
+
+export default function infoTable() {
+    return(
+        <div></div>
+    )
+}
