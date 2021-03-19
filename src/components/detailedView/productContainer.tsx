40a480343a0b6b8d2e0bf5c6ae27433b428db7d8
jv98 <jacob@vahlen.se>
2021-03-19 17:28:21 +0100

    removed old files

--- src/components/detailedView/productContainer.tsx
+++ src/components/detailedView/productContainer.tsx
@@ -5,87 +5,87 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import Button from '@material-ui/core/Button';
-import { Product } from '../mockedProducts';
-
-type Props = {
-   products: Product;
-   handleAddToCart: (products: Product) => void;
-};
+import { Product, products } from '../mockedProducts';
+import { useRouteMatch } from 'react-router-dom';
 
 const useStyles = makeStyles((theme: Theme) =>
-   createStyles({
-      root: {
-         flexGrow: 1,
-         height: '100vh',
-      },
-      paper: {
-         padding: theme.spacing(2),
-         margin: 'auto',
-         maxWidth: '50rem',
-         height: 'auto',
-         width: 'auto',
-      },
-      image: {
-         width: '20rem',
-         height: '20rem',
-      },
-      img: {
-         margin: 'auto',
-         display: 'block',
-         maxWidth: '100%',
-         maxHeight: '100%',
-      },
-   }),
+  createStyles({
+    root: {
+      flexGrow: 1,
+      height: '100vh',
+    },
+    paper: {
+      padding: theme.spacing(2),
+      margin: 'auto',
+      maxWidth: '60rem',
+      height: 'auto',
+      width: 'auto',
+    },
+    image: {
+      width: '20rem',
+      height: '20rem',
+    },
+    title:{
+      font: '2rem',
+      height: '4rem',
+    },
+  
+    img: {
+      margin: 'auto',
+      display: 'block',
+      maxWidth: '100%',
+      maxHeight: '100%',
+    },
+  }),
 );
+ 
+const ProductContainer: React.FC = () => {
+  const classes = useStyles();
+  const match = useRouteMatch<{ id: string }>();
 
-const ProductContainer: React.FC<Props> = ({ products, handleAddToCart }) => {
-   const classes = useStyles();
+  const product = products.find(p => String(p.id) === match.params.id)
 
-   return (
-      <div className={classes.root}>
-         <Paper className={classes.paper}>
-            <Grid container spacing={2}>
-               <Grid item>
-                  <ButtonBase className={classes.image}>
-                     <img
-                        className={classes.img}
-                        src="https://live.staticflickr.com/3727/9541317886_bbaf9957ed_b.jpg"
-                        alt="Dahlias"
-                     ></img>
-                  </ButtonBase>
-               </Grid>
-               <Grid item xs={12} sm container>
-                  <Grid item xs container direction="column" spacing={2}>
-                     <Grid item xs>
-                        <Typography gutterBottom variant="subtitle1">
-                           {products.title}
-                        </Typography>
-                        <Typography variant="body2" gutterBottom>
-                           {products.description}
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary">
-                           {products.price}
-                        </Typography>
-                     </Grid>
-                     <Grid item>
-                        <Typography
-                           variant="body2"
-                           style={{ cursor: 'pointer' }}
-                        >
-                           <Button
-                              onClick={() => handleAddToCart(products)}
-                              variant="contained"
-                              color="primary"
-                           >
-                              Lägg till kundvägen
-                           </Button>
-                        </Typography>
-                     </Grid>
-                  </Grid>
-               </Grid>
+  const handleAddToCart = () => {
+    // todo....
+  }
+
+  if (!product) {
+    return <p>"Produkt du letar efter finns inte... 404"</p>
+  }
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Grid item>
+            <ButtonBase className={classes.image}>
+            <img className={classes.img} src="https://live.staticflickr.com/3727/9541317886_bbaf9957ed_b.jpg" alt="Dahlias"></img>
+            </ButtonBase>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography className={classes.title} gutterBottom variant="h3" component="h5">
+                  {product.title}
+                </Typography>
+                <Typography variant="h6" gutterBottom component="h4">
+                 {product.description}
+                </Typography>
+                <Typography  variant="h6" color="textSecondary" component="p">
+                  {product.price} kr
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="body2" style={{ cursor: 'pointer' }}>
+                <Button onClick={handleAddToCart} variant="contained" color="primary">
+                 läg till kundvägen</Button> 
+                </Typography>
+              </Grid>
             </Grid>
-         </Paper>
-      </div>
-   );
-};
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+}
 export default ProductContainer;
