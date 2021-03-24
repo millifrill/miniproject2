44cae7d9446c544b2bd471d867a7a9a985fe1592
Camilla Johansson <camilla.johansson1986@gmail.com>
2021-03-24 15:46:57 +0100

    MenuItem value

--- src/components/masterView/productCard.tsx
+++ src/components/masterView/productCard.tsx
@@ -135,17 +135,17 @@ const ProductCard: React.FC<Props> = ({ product, handleAddToCart }) => {
                      <MenuItem value="">
                         <em>None</em>
                      </MenuItem>
-                     {/* <MenuItem value={10}>0</MenuItem> */}
+                     <MenuItem value={0}>0</MenuItem>
                      <MenuItem value={1}>1</MenuItem>
                      <MenuItem value={2}>2</MenuItem>
                      <MenuItem value={3}>3</MenuItem>
                      <MenuItem value={4}>4</MenuItem>
                      <MenuItem value={5}>5</MenuItem>
                      <MenuItem value={6}>6</MenuItem>
-                     <MenuItem value={30}>7</MenuItem>
-                     <MenuItem value={30}>8</MenuItem>
-                     <MenuItem value={30}>9</MenuItem>
-                     <MenuItem value={30}>10+</MenuItem>
+                     <MenuItem value={7}>7</MenuItem>
+                     <MenuItem value={8}>8</MenuItem>
+                     <MenuItem value={9}>9</MenuItem>
+                     <MenuItem value={10+}>10+</MenuItem>
                   </Select>
                </FormControl>
 
