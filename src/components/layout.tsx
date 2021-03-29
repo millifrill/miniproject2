8dcd8fff7eb71e81cd29200e2ed86f69b132dc6b
Camilla Johansson <camilla.johansson1986@gmail.com>
2021-03-29 08:47:32 +0200

    file / component rename

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,9 +5,10 @@ import Header from './header';
 import StartPageView from './masterView/startPageView';
 import DetailedView from './detailedView/detailedView';
 import checkoutView from './cartView/checkoutView';
+import CartView from './cartView/cartView';
 import ShippingVeiw from './cartView/shippingView';
-import AddressForm from './cartView/custumerInfoView';
-import PaymentForm from './cartView/paymentView';
+import AddressForm from './cartView/addressForm';
+import PaymentForm from './cartView/paymentForm';
 
 
 interface Props {}
@@ -25,7 +26,7 @@ export default class Layout extends Component<Props, State> {
                <Route path="/checkout" component={checkoutView} />
                <Route path="/cart" component={CartView} />
                <Route path="/shippingView" component={ShippingVeiw}/>
-               <Route path="/adress" component={AddressForm} />
+               <Route path="/address" component={AddressForm} />
                <Route path="/pay" component={PaymentForm} />
                <p>404 sidan finns inte...</p>
             </Switch>
