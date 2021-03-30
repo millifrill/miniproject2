3d09db7826faca1b39bd16c2cb1619f75c098258
Camilla Johansson <camilla.johansson1986@gmail.com>
2021-03-30 20:22:31 +0200

    same type of styling variables

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,31 +5,30 @@ import Toolbar from '@material-ui/core/Toolbar';
 // import SearchIcon from '@material-ui/icons/Search';
 import Typography from '@material-ui/core/Typography';
 import {
-   createStyles,
-   fade,
-   Theme,
-   makeStyles,
+    createStyles,
+    fade,
+    Theme,
+    makeStyles,
 } from '@material-ui/core/styles';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import Badge from '@material-ui/core/Badge';
-import { CSSProperties } from '@material-ui/styles';
 import Button from '@material-ui/core/Button';
 // import { CartContext } from '../contexts/cartContext';
 
 export default function Header() {
-   const classes = useStyles();
+    const classes = useStyles();
 
-   return (
-      // <CartContext.Consumer>
-         <div className={classes.root}>
+    return (
+        // <CartContext.Consumer>
+        <div className={classes.root}>
             <AppBar position="static">
-               <Toolbar>
-                  <Typography className={classes.title} variant="h6" noWrap>
-                     <Link to="/" style={noDecoration}>
-                        DAHLIABANKEN
-                     </Link>
-                  </Typography>
-                  {/* <div className={classes.search}>
+                <Toolbar>
+                    <Typography className={classes.title} variant="h6" noWrap>
+                        <Link to="/" className={classes.noDecoration}>
+                            DAHLIABANKEN
+                        </Link>
+                    </Typography>
+                    {/* <div className={classes.search}>
                   <div className={classes.searchIcon}>
                      <SearchIcon />
                   </div>
@@ -42,105 +41,105 @@ export default function Header() {
                      inputProps={{ 'aria-label': 'search' }}
                   />
                </div> */}
-                  <Button
-                     variant="contained"
-                     color="primary"
-                     className={classes.button}
-                     startIcon={
-                        <div>
-                           <Badge color="error" badgeContent={0} showZero>
-                              <ShoppingCartIcon />
-                           </Badge>
-                           <label className={classes.cartLabel}>Kundvagn</label>
-                        </div>
-                     }
-                  ></Button>
-                  <Button className={classes.button} variant="contained">
-                     Till kassan
-                  </Button>
-               </Toolbar>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        className={classes.button}
+                        startIcon={
+                            <div>
+                                <Badge color="error" badgeContent={0} showZero>
+                                    <ShoppingCartIcon />
+                                </Badge>
+                                <label className={classes.cartLabel}>
+                                    Kundvagn
+                                </label>
+                            </div>
+                        }
+                    ></Button>
+                    <Button className={classes.button} variant="contained">
+                        Till kassan
+                    </Button>
+                </Toolbar>
             </AppBar>
-         </div>
-      // </CartContext.Consumer>
-   );
+        </div>
+        // </CartContext.Consumer>
+    );
 }
 
-
 const useStyles = makeStyles((theme: Theme) =>
-   createStyles({
-      root: {
-         flexGrow: 1,
-      },
-      menuButton: {
-         marginRight: theme.spacing(2),
-         marginStart: '2rem',
-      },
-      shoppingCartButton: {
-         marginStart: '1rem',
-      },
-      title: {
-         flexGrow: 1,
-         marginStart: '2rem',
-         display: 'none',
-         [theme.breakpoints.up('sm')]: {
-            display: 'block',
-         },
-      },
-      search: {
-         position: 'relative',
-         borderRadius: theme.shape.borderRadius,
-         backgroundColor: fade(theme.palette.common.white, 0.15),
-         '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
-         },
-         marginLeft: '1rem',
-         marginRight: '1rem',
-         width: '100%',
-         [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(1),
-            width: 'auto',
-         },
-      },
-      searchIcon: {
-         padding: theme.spacing(0, 2),
-         height: '100%',
-         position: 'absolute',
-         pointerEvents: 'none',
-         display: 'flex',
-         alignItems: 'center',
-         justifyContent: 'center',
-      },
-      inputRoot: {
-         color: 'inherit',
-      },
-      button: {
-         height: '2.5rem',
-         marginLeft: '2rem',
-         background: 'lightBlue',
-         color: 'black',
-         backgroundColor: '#a7c5f1',
-         fontSize: '0.8rem',
-      },
-      cartLabel: {
-         marginLeft: '1rem',
-      },
-      inputInput: {
-         padding: theme.spacing(1, 1, 1, 0),
-         // vertical padding + font size from searchIcon
-         paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-         transition: theme.transitions.create('width'),
-         width: '100%',
-         [theme.breakpoints.up('sm')]: {
-            width: '12ch',
-            '&:focus': {
-               width: '20ch',
+    createStyles({
+        root: {
+            flexGrow: 1,
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+            marginStart: '2rem',
+        },
+        shoppingCartButton: {
+            marginStart: '1rem',
+        },
+        title: {
+            flexGrow: 1,
+            marginStart: '2rem',
+            display: 'none',
+            [theme.breakpoints.up('sm')]: {
+                display: 'block',
             },
-         },
-      },
-   }),
+        },
+        noDecoration: {
+            textDecoration: 'none',
+            color: 'white',
+        },
+        search: {
+            position: 'relative',
+            borderRadius: theme.shape.borderRadius,
+            backgroundColor: fade(theme.palette.common.white, 0.15),
+            '&:hover': {
+                backgroundColor: fade(theme.palette.common.white, 0.25),
+            },
+            marginLeft: '1rem',
+            marginRight: '1rem',
+            width: '100%',
+            [theme.breakpoints.up('sm')]: {
+                marginLeft: theme.spacing(1),
+                width: 'auto',
+            },
+        },
+        searchIcon: {
+            padding: theme.spacing(0, 2),
+            height: '100%',
+            position: 'absolute',
+            pointerEvents: 'none',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        inputRoot: {
+            color: 'inherit',
+        },
+        button: {
+            height: '2.5rem',
+            marginLeft: '2rem',
+            background: 'lightBlue',
+            color: 'black',
+            backgroundColor: '#a7c5f1',
+            fontSize: '0.8rem',
+        },
+        cartLabel: {
+            marginLeft: '1rem',
+        },
+        inputInput: {
+            padding: theme.spacing(1, 1, 1, 0),
+            // vertical padding + font size from searchIcon
+            paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+            transition: theme.transitions.create('width'),
+            width: '100%',
+            [theme.breakpoints.up('sm')]: {
+                width: '12ch',
+                '&:focus': {
+                    width: '20ch',
+                },
+            },
+        },
+    }),
 );
-
-const noDecoration: CSSProperties = {
-   textDecoration: 'none',
-   color: 'white',
-};
