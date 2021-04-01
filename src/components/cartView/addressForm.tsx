2f0ea9de5b33fbf90c628e864b1ad1536640e627
Camilla Johansson <camilla.johansson1986@gmail.com>
2021-04-01 17:15:25 +0200

    errors atm

--- src/components/cartView/addressForm.tsx
+++ src/components/cartView/addressForm.tsx
@@ -5,25 +5,77 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-// import { useForm } from 'react-hook-form';
+import { Form, useForm } from './useForm';
 // import { SettingsPhone } from '@material-ui/icons';
 
-export default function AddressForm() {
+type Props = {
+    id: number;
+    fistName: string;
+    lastName: string;
+    adress: string;
+    city: string;
+    regin: string;
+    zip: string;
+    contry: string;       
+    moblie: string;       
+    email: string;    
+}
+
+export default function AddressForm () { 
+    
     const classes = useStyles();
    //  const { handleSubmit, errors } = useForm();
-   //  const onSubmit = data => console.log(data);
-   //  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-   //     const { target: { value } } = event;
-   //     setErrors({ phone: '' })
-   //     SettingsPhone(value);
-   //     let reg = new RegExp(/^\)
-   //  }
+
+   const initialFormValues = {
+       id: 0,
+       fistName: '',
+       lastName: '',
+       adress: '',
+       city: '',
+       regin: '',
+       zip: '',
+       contry: '',       
+       moblie: '',       
+       email: '',       
+   }
+
+   const validate = (fieldValues = values) => {
+       let temp = {...errors} 
+        if ('firstName' in fieldValues)
+            temp.firstName = fieldValues.firstName ? '' : 'This field is required.'
+        if ('lastName' in fieldValues)
+            temp.lastName = fieldValues.lastName ? '' : 'This field is required.'
+        if ('email' in fieldValues)
+            temp.email = (/§^|.+@.+..+/).test(fieldValues.email) ? '' : 'Email is not valid.'
+        if ('mobile' in fieldValues)
+            temp.mobile = fieldValues.mobile.length> 9 ? '' : 'Telephone number is not valid.'
+        setErrors({
+            ...temp
+        })
+        if (fieldValues == values)
+        return Object.values(temp).every(x => == '')
+    }
+
+   const {
+        values,
+        setValues,
+        errors,
+        setErrors,
+        handleInputChange,
+        // resetForm
+    } = useForm(initialFormValues, true, validate);
+
+    const handleSubmit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault()
+        if (validate())
+    }
 
     return (
         <React.Fragment>
             <Typography variant="h5" gutterBottom>
                 Kunduppgifter
             </Typography>
+        <Form onSubmit={handleSubmit}>
             <Grid className={classes.form} container spacing={3}>
                 <Grid item xs={12} sm={6}>
                     <TextField
@@ -31,11 +83,12 @@ export default function AddressForm() {
                         id="firstName"
                         name="firstName"
                         label="Förnamn"
+                        value={values.firstName}
                         fullWidth
                         autoComplete="given-name"
-                  //       onChange={handleChange}
-                  //       error={Boolean(errors?.firstName)}
-                  //       helperText={(errors?.firstName)}
+                        onChange={handleInputChange}
+                        error={errors.firstName}
+                        helperText={(errors?.firstName)}
                     />
                 </Grid>
                 <Grid item xs={12} sm={6}>
@@ -44,8 +97,11 @@ export default function AddressForm() {
                         id="lastName"
                         name="lastName"
                         label="Efternamn"
+                        value={values.lastName}
                         fullWidth
                         autoComplete="family-name"
+                        onChange={handleInputChange}
+                        error={errors.lastName}
                     />
                 </Grid>
                 <Grid item xs={12}>
@@ -54,8 +110,11 @@ export default function AddressForm() {
                         id="address1"
                         name="address1"
                         label="Address"
+                        value={values.adress}
                         fullWidth
                         autoComplete="shipping address-line1"
+                        onChange={handleInputChange}
+                        error={errors.adress}
                     />
                 </Grid>
                 <Grid item xs={12}>
@@ -63,8 +122,11 @@ export default function AddressForm() {
                         id="address2"
                         name="address2"
                         label="C/O"
+                        value={values.adress}
                         fullWidth
                         autoComplete="shipping address-line2"
+                        onChange={handleInputChange}
+                        error={errors.adress}
                     />
                 </Grid>
                 <Grid item xs={12} sm={6}>
@@ -73,17 +135,23 @@ export default function AddressForm() {
                         id="city"
                         name="city"
                         label="Ort"
+                        value={values.city}
                         fullWidth
                         autoComplete="shipping address-level2"
+                        onChange={handleInputChange}
+                        error={errors.city}
                     />
                 </Grid>
                 <Grid item xs={12} sm={6}>
                     <TextField
                         required
-                        id="state"
-                        name="state"
+                        id="region"
+                        name="region"
                         label="Region"
+                        value={values.region}
                         fullWidth
+                        onChange={handleInputChange}
+                        error={errors.region}
                     />
                 </Grid>
                 <Grid item xs={12} sm={6}>
@@ -92,8 +160,11 @@ export default function AddressForm() {
                         id="zip"
                         name="zip"
                         label="Postkod"
+                        value={values.zip}
                         fullWidth
                         autoComplete="shipping postal-code"
+                        onChange={handleInputChange}
+                        error={errors.zip}
                     />
                 </Grid>
                 <Grid item xs={12} sm={6}>
@@ -102,8 +173,11 @@ export default function AddressForm() {
                         id="country"
                         name="country"
                         label="Land"
+                        value={values.country}
                         fullWidth
                         autoComplete="shipping country"
+                        onChange={handleInputChange}
+                        error={errors.country}
                     />
                 </Grid>
                 <Grid item xs={12} sm={6}>
@@ -112,8 +186,11 @@ export default function AddressForm() {
                         id="phoneNumber"
                         name="phoneNumber"
                         label="Telefon nummer"
+                        value={values.mobile}
                         fullWidth
                         autoComplete="tel"
+                        onChange={handleInputChange}
+                        error={errors.mobile}
                     />
                 </Grid>
                 <Grid item xs={12} sm={6}>
@@ -122,8 +199,11 @@ export default function AddressForm() {
                         id="mail"
                         name="mail"
                         label="Mail"
+                        value={values.email}
                         fullWidth
                         autoComplete="email"
+                        onChange={handleInputChange}
+                        error={errors.email}
                     />
                 </Grid>
                 <Grid item xs={12}>
@@ -139,6 +219,7 @@ export default function AddressForm() {
                     />
                 </Grid>
             </Grid>
+        </Form>
         </React.Fragment>
     );
 }
---
+++ src/components/cartView/useForm.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import { makeStyles } from '@material-ui/core';
+
+type Props = {
+    id: number;
+    fistName: string;
+    lastName: string;
+    adress: string;
+    city: string;
+    regin: string;
+    zip: string;
+    contry: string;       
+    moblie: string;       
+    email: string;
+    validate: boolean;    
+}
+
+export function useForm(initialFormValues, validateOnChange = false, validate) {
+    const [values, setValues] = useState(initialFormValues);
+    const [errors, setErrors] = useState({});
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setValues({
+            ...values,
+            [name]: value,
+        });
+        if (validateOnChange) validate({ [name]: value });
+    };
+
+    // const resetForm = () => {
+    //     setValues(initialFormValues);
+    //     setErrors({});
+    // };
+
+    return {
+        values,
+        setValues,
+        errors,
+        setErrors,
+        handleInputChange,
+        // resetForm,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& .MuiFormControl-root': {
+            width: '80%',
+            margin: theme.spacing(1),
+        },
+    },
+}));
+
+export function Form(props) {
+    const classes = useStyles();
+    const { children, ...other } = props;
+    return (
+        <form className={classes.root} autoComplete="on" {...other}>
+            {props.children}
+        </form>
+    );
+}
